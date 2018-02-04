97cdbddad47e45edde6dfcd05deac3250f9ee79b
Poulad <poulad.pld@gmail.com>
2018-02-04 05:05:55 -0500

    Complete lab 02

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public GameObject Character;
+
+    public Sprite Sprite;
+
+    private SpriteRenderer _spriteRenderer;
+
+    void OnMouseDown()
+    {
+        if (_spriteRenderer == null)
+        {
+            _spriteRenderer = Character.GetComponent<SpriteRenderer>();
+        }
+
+        _spriteRenderer.sprite = Sprite;
+    }
+}
